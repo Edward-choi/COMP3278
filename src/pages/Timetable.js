2a1fa194d0a21ad68c136d7a555a50897ec967f0
Edward-choi <u3568441@connect.hku.hk>
2022-10-28 21:31:27 +0800

    Revert "tiemtable"
    This reverts commit 6d8d010819b0a6da204e59ed177e1e781470c998.

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -1,28 +1,13 @@
 import MainContent from "../shared/MainContent";
-import { appointments } from "../demo-data/appointments";
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import {
-    Scheduler,
-    WeekView,
-    Appointments,
-} from '@devexpress/dx-react-scheduler-material-ui';
 
 function Timetable() {
     return (
         <div>
             <MainContent>
                 <h1>Timetable</h1>
-                <Paper>
-                    <Scheduler data={appointments} height={660}>
-                        <WeekView startDayHour={9} endDayHour={19} />
-                        <Appointments />
-                    </Scheduler>
-                </Paper>
             </MainContent>
         </div>
     );
 }
 
 export default Timetable;
-
