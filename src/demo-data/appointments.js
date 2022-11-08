cbcd406f2cc9409c6dc602405825249758c8b193
Edward-choi <u3568441@connect.hku.hk>
2022-11-08 23:35:16 +0800

    change timetable

--- src/demo-data/appointments.js
+++ src/demo-data/appointments.js
@@ -7,13 +7,13 @@ let year = today.getFullYear();
 
 export const appointments = [
   {
-    title: <div className="courseInfo"><h3 className="courseCode">COMP3278</h3><div><LocationOnIcon sx={{marginBottom:-2, width: 15}} />MB301</div></div>,
+    title: <div className="courseInfo"><h4 className="courseCode">COMP3278</h4><div><LocationOnIcon sx={{marginBottom:-2, width: 15}} />MB301</div></div>,
     startDate: new Date(year, month, day, 9, 30),
     endDate: new Date(year, month, day, 11, 30),
     id: 0,
     location: 'Room 1',
   }, {
-    title: <div className="courseInfo"><h3 className="courseCode">COMP3330</h3><div><LocationOnIcon sx={{marginBottom:-2, width: 15}} />CPD LG08</div></div>,
+    title: <div className="courseInfo"><h4 className="courseCode">COMP3330</h4><div><LocationOnIcon sx={{marginBottom:-2, width: 15}} />CPD LG08</div></div>,
     startDate: new Date(year, month, day - 1, 12, 0),
     endDate: new Date(year, month, day - 1, 12, 50),
     id: 1,
--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -1,22 +1,44 @@
 import MainContent from "../shared/MainContent";
 import { appointments } from "../demo-data/appointments";
-import * as React from 'react';
+import React, { useState } from 'react';
 import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
     WeekView,
     Appointments,
+    Toolbar,
+    DateNavigator,
+    TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 function Timetable() {
+    const today = new Date();
+
+    let day = today.getDate();
+    let month = today.getMonth()+1;
+    let year = today.getFullYear();
+    const now = `${year}-${month<10?`0${month}`:`${month}`}-$${day<10?`0${day}`:`${day}`}`;
+    const [date, setDate] = useState(now);
     return (
         <div>
             <MainContent>
                 <h1>Timetable</h1>
                 <Paper>
                     <Scheduler data={appointments} height={660}>
-                        <WeekView startDayHour={8} endDayHour={19} cellDuration={30}/>
-                        <Appointments/>
+                        <ViewState
+                            currentDate={date}
+                            onCurrentDateChange={newDate => setDate(newDate)}
+                        />
+                        <WeekView
+                            startDayHour={8}
+                            endDayHour={19}
+                            cellDuration={30}
+                        />
+                        <Toolbar />
+                        <DateNavigator />
+                        <TodayButton />
+                        <Appointments />
                     </Scheduler>
                 </Paper>
             </MainContent>
